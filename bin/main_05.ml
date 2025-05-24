e6f27779f72d825c56b9c337c9b6091047993b57
simondanielsson <simon.danielsson99@hotmail.com>
2025-05-24 15:41:42 +0200

    Day 5 part 2

--- bin/main_05.ml
+++ bin/main_05.ml
@@ -31,88 +31,86 @@ let dfs_match ~graph page others : bool =
     let neighbors = Hashtbl.find_multi graph current in
     List.fold neighbors ~init:visited ~f:(fun acc neighbor ->
       if not (List.mem acc neighbor ~equal:String.equal)
-      then (
-        printf "Visiting neighbor %s from current %s\n" neighbor current;
-        dfs (neighbor :: acc) neighbor)
+      then dfs (neighbor :: acc) neighbor
       else acc)
   in
   let rec aux found page others =
     match others with
     | fst :: rst ->
-      printf "\n-- Running dfs on page %s --\n\n" page;
       let deps_of_page = dfs [] page in
-      printf "Dependencies of page %s:\n" page;
-      print_s [%sexp (deps_of_page : string list)];
       if List.mem deps_of_page fst ~equal:String.equal then false else aux found page rst
     | [] -> found
   in
   aux true page others
 ;;
 
+(* Part 1*)
 let string_in_list lst value = List.mem lst value ~equal:String.equal
 
-let no_inverse_dependencies ~deps pages =
-  (* Create filtered adjacency graph.
-     Consider only dependencies that relate to the update in question *)
+let build_inverse_deps_for_pages ~deps pages =
   let graph = Hashtbl.create (module String) in
   List.filter deps ~f:(fun (from, to_) ->
     string_in_list pages from && string_in_list pages to_)
   |> List.iter ~f:(fun (from, to_) -> Hashtbl.add_multi graph ~key:to_ ~data:from);
+  graph
+;;
+
+let no_inverse_dependencies ~deps pages =
+  (* Create filtered adjacency graph.
+     Consider only dependencies that relate to the update in question *)
+  let graph = build_inverse_deps_for_pages ~deps pages in
   let rec aux = function
     | page :: others -> dfs_match ~graph page others && aux others
     | [] -> true
   in
-  printf "Checking deps of pages:\n";
-  print_s [%sexp (pages : string list)];
-  let had_no_dependencies = aux pages in
-  printf "had_no_deps=%s\n" (Bool.to_string had_no_dependencies);
-  had_no_dependencies
+  aux pages
 ;;
 
 let find_valid_updates (deps, updates) =
-  print_s [%sexp (deps : (string * string) list)];
-  print_s [%sexp (updates : string list list)];
-  (* Filter updates that are valid *)
   List.filter updates ~f:(no_inverse_dependencies ~deps)
   |> List.map ~f:(List.map ~f:Int.of_string)
 ;;
 
-(*
-   For each update
-1. Filter out the deps that include two page from update
-2. Build (inverse) dependency graph (adjacency matrix through hashtbl)
-3. For each page
-      For each subsequent page
-          if page depends on subsequent page 
-            return acc
-   pages :: acc
-
-
-helper page_depends_on_other graph page others =
-  // perform bfs
-  let dfs visited found page = 
-    for each neighbor of page
-      if neighbor not visited 
-        dfs (neighbor::visited) found neighbor
-      else 
-        visited, found
-  let dependencies_of_page, found = dfs [] false page in
-  found
-*)
-
 let get_medians updates =
-  printf "Calculating medians for %d updates\n" (List.length updates);
   List.fold updates ~init:[] ~f:(fun acc pages ->
     let median = List.nth pages (List.length pages / 2) |> Option.value_exn in
     median :: acc)
 ;;
 
-let sum lst =
-  printf "Summing medians of %d lists\n" (List.length lst);
-  List.fold lst ~init:0 ~f:(fun acc x -> x + acc)
+let sum lst = List.fold lst ~init:0 ~f:(fun acc x -> x + acc)
+
+(* Part 2 *)
+let depends_on ~graph page other = dfs_match ~graph page [ other ]
+
+let fix_update ~deps update =
+  let graph = build_inverse_deps_for_pages ~deps update in
+  ignore deps;
+  let update_arr = Array.of_list update in
+  let length = Array.length update_arr - 1 in
+  for start_index = length downto 0 do
+    let curr_index = ref start_index in
+    for remain_index = start_index + 1 to length do
+      if depends_on ~graph update_arr.(remain_index) update_arr.(!curr_index)
+      then (
+        Array.swap update_arr !curr_index remain_index;
+        curr_index := remain_index)
+    done
+  done;
+  update_arr |> List.of_array
+;;
+
+let correct_invalid_updates (deps, updates) =
+  List.filter updates ~f:(fun update -> not (no_inverse_dependencies ~deps update))
+  |> List.map ~f:(fix_update ~deps)
+  |> List.map ~f:(List.map ~f:Int.of_string)
 ;;
 
 let () =
-  let result = read () |> find_valid_updates |> get_medians |> sum in
-  printf "Result: %d" result
+  ignore find_valid_updates;
+  (* Part 1 *)
+  (* let result_part1 = read () |> find_valid_updates |> get_medians |> sum in *)
+  (* printf "Result: %d\n" result_part1; *)
+  (* Part 2*)
+  let result_part2 = read () |> correct_invalid_updates |> get_medians |> sum in
+  printf "Result: %d\n" result_part2
 ;;
