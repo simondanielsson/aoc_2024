76d8bdc76e3c99079324310b641e59c72b2157a2
simondanielsson <simon.danielsson99@hotmail.com>
2025-07-17 22:33:07 +0200

    Day 16 p2

--- bin/main_16.ml
+++ bin/main_16.ml
@@ -50,18 +50,14 @@ module PosWithDist = struct
   type t =
     { pos : Pos.t
     ; dist : int
-    ; parent : IntPair.t
+    ; parent : Pos.t
     }
   [@@deriving sexp_of]
 
   let with_total_dist (cur : t) (neighbor : Pos.t) =
     if Dir.equal cur.pos.dir neighbor.dir
-    then
-      { pos = neighbor
-      ; dist = cur.dist + Constants.straight_weight
-      ; parent = cur.pos.pos
-      }
-    else { pos = neighbor; dist = cur.dist + Constants.turn_weight; parent = cur.pos.pos }
+    then { pos = neighbor; dist = cur.dist + Constants.straight_weight; parent = cur.pos }
+    else { pos = neighbor; dist = cur.dist + Constants.turn_weight; parent = cur.pos }
   ;;
 
   let compare_dist t1 t2 = Int.compare t1.dist t2.dist
@@ -156,7 +152,7 @@ let shortest_path (g, start, ends) =
       let next = Pairing_heap.pop heap |> Option.value_exn in
       dijkstras next)
   in
-  dijkstras PosWithDist.{ pos = start; dist = 0; parent = start.pos }
+  dijkstras PosWithDist.{ pos = start; dist = 0; parent = start }
 ;;
 
 (* Part 2
@@ -174,55 +170,87 @@ let shortest_path (g, start, ends) =
 let shortest_path_part2 (g, start, ends) =
   let visited = ref [] in
   let heap = Pairing_heap.create ~cmp:PosWithDist.compare_dist () in
-  let parents = Hashtbl.create (module IntPair) in
-  let rec dijkstras (cur : PosWithDist.t) (cur_best : PosWithDist.t) =
+  let parents = Hashtbl.create (module Pos) in
+  let best = Hashtbl.create (module IntPair) in
+  let rec dijkstras (cur : PosWithDist.t) =
+    if fst cur.pos.pos = 6 && snd cur.pos.pos = 15 then print_endline "6 - 15";
+    if fst cur.pos.pos = 7 && snd cur.pos.pos = 15 then print_endline "7 - 15";
     if List.mem !visited cur.pos ~equal:Pos.equal
     then (
       let next = Pairing_heap.pop heap |> Option.value_exn in
-      print_endline "Already visited";
-      dijkstras next cur_best)
+      printf
+        "Already visited. cur=%s. next: %s. cdtc=%d\n"
+        (PosWithDist.sexp_of_t cur |> Sexp.to_string)
+        (PosWithDist.sexp_of_t next |> Sexp.to_string)
+        (Hashtbl.find_exn best cur.pos.pos);
+      (* Add as parent if this is another way to get to current node, modulo one turn *)
+      let best_distance_to_cur = Hashtbl.find_exn best cur.pos.pos in
+      if cur.dist <= best_distance_to_cur + Constants.turn_weight
+      then (
+        printf "adding ctdc";
+        Hashtbl.add_multi parents ~key:cur.pos ~data:cur.parent);
+      dijkstras next)
     else if List.exists ends ~f:(fun _end -> Pos.same_pos cur.pos _end)
     then cur.dist
     else (
       visited := cur.pos :: !visited;
+      if not (Hashtbl.mem best cur.pos.pos)
+      then Hashtbl.add_exn best ~key:cur.pos.pos ~data:cur.dist;
       let neighbors = Hashtbl.find_exn g cur.pos in
       List.iter neighbors ~f:(fun neighbor ->
         Pairing_heap.add heap (PosWithDist.with_total_dist cur neighbor));
       let next = Pairing_heap.pop heap |> Option.value_exn in
-      let parents_of_next = Hashtbl.find_multi parents next.pos.pos in
+      let parents_of_next = Hashtbl.find_multi parents next.pos in
       (* TODO: we need to add parents if they have the same dist modulo 1-2 turns! *)
       (* If the parent is already in the list of parents to the node, don't do anythin*)
       if not
-           (List.mem parents_of_next next.parent ~equal:IntPair.equal
-            || IntPair.equal next.parent next.pos.pos
+           (* Don't add duplicates (ignore direction)*)
+           (List.mem parents_of_next next.parent ~equal:Pos.equal
             || List.mem !visited next.pos ~equal:Pos.equal)
       then (
         printf "Assigning  ";
-        print_s [%sexp (next.parent : IntPair.t)];
+        print_s [%sexp (next.parent : Pos.t)];
         printf " as parent to ";
-        print_s [%sexp (next.pos.pos : IntPair.t)];
-        Hashtbl.add_multi parents ~key:next.pos.pos ~data:next.parent;
-        let new_parents = Hashtbl.find_multi parents next.pos.pos in
-        print_s [%sexp (new_parents : IntPair.t list)]);
-      dijkstras next cur_best)
+        print_s [%sexp (next.pos : Pos.t)];
+        Hashtbl.add_multi parents ~key:next.pos ~data:next.parent;
+        let new_parents = Hashtbl.find_multi parents next.pos in
+        print_s [%sexp (new_parents : Pos.t list)])
+      else printf "Nothing \n";
+      dijkstras next)
   in
-  let s = PosWithDist.{ pos = start; dist = 0; parent = start.pos } in
-  Hashtbl.add_multi parents ~key:start.pos ~data:start.pos;
-  let _ = dijkstras s s in
-  let traverse_visited = ref [] in
-  let rec traverse_path (pos : IntPair.t) count =
+  let s = PosWithDist.{ pos = start; dist = 0; parent = start } in
+  Hashtbl.add_multi parents ~key:start ~data:start;
+  let _ = dijkstras s in
+  printf "Parents:\n";
+  print_s [%sexp (parents : (Pos.t, Pos.t list) Hashtbl.t)];
+  let merged_parents = Hashtbl.create (module IntPair) in
+  let merge_parents () =
+    Hashtbl.iteri parents ~f:(fun ~key ~data ->
+      List.iter data ~f:(fun parent ->
+        Hashtbl.add_multi merged_parents ~key:key.pos ~data:parent.pos))
+  in
+  print_endline "Merged parents:";
+  merge_parents ();
+  print_s [%sexp (merged_parents : (IntPair.t, IntPair.t list) Hashtbl.t)];
+  let traverse_visited : IntPair.t list ref = ref [] in
+  let rec traverse_path (pos : Pos.t) count =
     let cur_parents = Hashtbl.find_multi parents pos in
     List.fold cur_parents ~init:count ~f:(fun acc parent ->
-      if not (List.mem !traverse_visited parent ~equal:IntPair.equal)
+      printf "Trying parent (%d, %d)\n" (fst parent.pos) (snd parent.pos);
+      if not (List.mem !traverse_visited parent.pos ~equal:IntPair.equal)
       then (
         printf "(%d) " acc;
-        print_s [%sexp (parent : IntPair.t)];
+        print_s [%sexp (parent : Pos.t)];
+        traverse_visited := parent.pos :: !traverse_visited;
         traverse_path parent (acc + 1))
-      else acc)
+      else traverse_path parent acc)
   in
-  printf "Parents:\n";
-  print_s [%sexp (parents : (IntPair.t, IntPair.t list) Hashtbl.t)];
-  traverse_path (List.hd_exn ends).pos 1
+  let relevant_ends =
+    List.map ends ~f:(fun _end -> if Hashtbl.mem parents _end then Some _end else None)
+  in
+  let _end = List.hd_exn @@ List.filter_opt relevant_ends in
+  print_s [%sexp (_end : Pos.t)];
+  traverse_path _end 1
 ;;
 
 let () =
